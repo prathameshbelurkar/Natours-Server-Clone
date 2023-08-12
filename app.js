1decf04a9e4f43a0a7972e3ea364b28bb4c8f365
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-08-12 17:21:06 +0530

    api/v1: add request for tours

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.status(200).json({
-    status: 'success',
-    message: 'Hello from the server side!',
-  });
-});
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'))
+);
 
-app.post('/', (req, res) => {
-  return res.status(200).json({
-    status: 'success',
-    message: 'Hello POST Req Here!',
-  });
+app.get('/api/v1/tours', (req, res) => {
+  return res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
 const port = 3000;
