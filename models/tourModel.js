68a6be86f0538835f6511fdaec3eed657245dd10
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-09-03 14:57:38 +0530

    added aggr. middleware in model

--- models/tourModel.js
+++ models/tourModel.js
@@ -93,6 +93,16 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: {
+      secretTour: { $ne: true },
+    },
+  });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
