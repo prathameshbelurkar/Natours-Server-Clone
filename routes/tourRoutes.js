01e28a65a73f33b3b3082aecf1014512417e87da
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-09-03 10:46:00 +0530

    add route 'monthly-plan': add aggr. pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,16 @@ const tourController = require('../controllers/tourController');
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
+
 router.route('/tour-stats').get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
